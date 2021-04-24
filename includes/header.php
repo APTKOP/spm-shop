4e3da611f3f9ddfbf7370b7860455c6e88ca033d
APTKOP <52624723+APTKOP@users.noreply.github.com>
2021-04-24 18:18:10 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<?php
+$nickname = "Ваш ник";
+$name = "Название магазина"; // пока нигде не используется
+$discord = "Ссылка на дискорд сервер";
+$wallet = "Имя счёта в банке СПм"
+?>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="https://minotar.net/helm/<?=$nickname?>/16.png">
+  <title><?=$nickname?> Shop</title>
+  <link href="css/style.css" media="screen" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+  <meta name="og:site_name" content="Магазин <?=$nickname?>" /> <!-- Цвет темы сайта -->
+  <meta charset="utf-8" />
+  <meta name="og:type" content="object" />
+  <meta
+    name="og:image"
+    content="https://minotar.net/helm/<?=$nickname?>/55"
+  />
+  <meta name="theme-color" content="#FA7B00" /> <!-- Цвет темы сайта -->
+ </head>
+ <body>
